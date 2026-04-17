22936a5ed04362ae63b15c1030f61ed08baa49ab
Wagner Senai <wagnercorp17@gmail.com>
2026-04-16 21:12:49 -0300

    Funcao para adicionar e exibir tarefas criada e funcionando corretamente

--- docs/diario-de-bordo.md
+++ docs/diario-de-bordo.md
@@ -2,6 +2,7 @@ Atualizado diariamente com:
  O que foi feito
  15/04 - Criado arquivos iniciais do projeto
         Criado HTML inicial
+ 16/04 - Iniciada lógica em JavaScript
  Dificuldades encontradas
  Soluções aplicadas
  O que será feito no próximo dia
--- src/main.js
+++ src/main.js
@@ -2,3 +2,25 @@
 // um projeto em HTML/CSS/JS, Python, ou até um projeto apenas textual/documentado.
 // O foco não é a complexidade do código, mas a forma como o projeto é
 //   organizado, versionado e gerenciado.
+
+let tasks = [];
+
+function addTask() {
+    let taskInput = document.getElementById("taskInput").value;
+    console.log("Tarefa adicionada:", taskInput);
+    if (taskInput == null || taskInput == "") {
+        alert("Por favor, insira uma tarefa válida.");
+        return;
+    }
+    tasks.push(taskInput);
+    console.log("Tarefas atuais:", tasks)
+
+    showTasks();
+}
+
+function showTasks() {
+    let tasksList = document.getElementById("tasks-list");
+
+    tasksList.innerHTML = tasks.map((task, index) => `<li id='${index}'>${task}</li>`).join("");
+
+}
